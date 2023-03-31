52591cc70f1bedb4731b9d792a8419fb63211738
José Antonio Trejo Gutiérrez <jose.trejog@sat.gob.mx>
2023-03-30 20:35:33 -0600

    The first component is created

---
+++ src/components/Testimonial.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../stylesheets/Testimonial.css"
+
+function Testimonial() {
+  return (
+    <div className='container-testimonial'>
+      <img
+        className='image-testimonial'
+        src={require('../images/fry.jpg')}
+        alt='Fry picture'/> 
+      <div className='testimonial_text_container'>
+        <p className='testimonial_name'>FRy from the future</p>
+        <p className='testimonial_ocupation'> delivery </p>
+        <p className='testimonial_text' >Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
