6eb5742e3de52d33ba0a99fb3736a3ce552325e8
José Antonio Trejo Gutiérrez <jose.trejog@sat.gob.mx>
2023-03-30 20:54:03 -0600

    The first prop was added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <div className='main-container'>
         <h1>This is the title</h1>
-        <Testimonial/>
+        <Testimonial
+          image='fry.jpg'
+          name='Fry'
+          time='Future'
+          ocupation='Delivery'
+          testimonial='Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla'
+          />
       </div>
 
     </div>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import "../stylesheets/Testimonial.css"
 
-function Testimonial() {
+function Testimonial(props) {
   return (
     <div className='container-testimonial'>
       <img
         className='image-testimonial'
-        src={require('../images/fry.jpg')}
+        src={require(`../images/${props.image}`)}
         alt='Fry picture'/> 
       <div className='testimonial_text_container'>
-        <p className='testimonial_name'>FRy from the future</p>
-        <p className='testimonial_ocupation'> delivery </p>
-        <p className='testimonial_text' >Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla</p>
+        <p className='testimonial_name'>{props.name} from the {props.time}</p>
+        <p className='testimonial_ocupation'> {props.ocupation} </p>
+        <p className='testimonial_text' >"{props.testimonial}"</p>
       </div>
     </div>
   );
